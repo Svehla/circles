5f13001fd60876c95357da0c2bfb4e5a4c71b120
Jakub Švehla <svehlj01@dixonscarphone.com>
2022-09-07 13:36:23 +0200

    circles

---
+++ src/analyzeSignal.ts
@@ -0,0 +1,449 @@
+import { distance } from './math'
+import { xd } from './renderFFTEngine'
+var ft = require('fourier-transform')
+
+type Point = {
+  x: number
+  y: number
+}
+
+// --------------------------- canvas utils ---------------------------
+const drawLine = (point1: Point, point2: Point, { color = 'black', width = 2 } = {}) => {
+  ctx.beginPath()
+  ctx.moveTo(point1.x, point1.y)
+  ctx.lineTo(point2.x, point2.y)
+  ctx.lineWidth = width
+  ctx.strokeStyle = color
+  ctx.stroke()
+}
+
+const drawRect = (point: Point, width: number, height: number, { color = 'black' } = {}) => {
+  ctx.beginPath()
+  ctx.rect(point.x, point.y, width, height)
+  ctx.fillStyle = color
+  ctx.fill()
+}
+
+const drawPoint = (point: Point, { color = 'black', size = 2 } = {}) => {
+  drawRect(point, size, size, { color })
+  // const _size = size === 1 ? 2 : size
+  // // if (size > 1) {
+  // // ctx.fillStyle = color
+  // ctx.rect(point.x, point.y, _size, _size)
+  // ctx.strokeStyle = color
+  // // ctx.fill()
+  // ctx.stroke()
+  // } else {
+  //   drawLine(point, { x: point.x + size, y: point.y + size }, { color, width: 1 })
+  // }
+}
+
+const drawCircle = (center: Point, radius: number, { color = 'black', width = 2 } = {}) => {
+  ctx.beginPath()
+  ctx.arc(center.x, center.y, radius, 0, 2 * Math.PI)
+  ctx.strokeStyle = color
+  ctx.lineWidth = width
+  ctx.stroke()
+}
+
+const view = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+const clearCanvas = () => ctx.clearRect(0, 0, view.width, view.height)
+
+var c = document.getElementById('myCanvas')!
+
+const setCanvasSize = () => {
+  // @ts-expect-error
+  c.width = view.width
+  // @ts-expect-error
+  c.height = view.height
+}
+
+// @ts-expect-error
+var ctx = c.getContext('2d')
+// ------------------------------------------------------------
+
+// configure the space
+
+const image1 = [
+  //
+  [100, 150],
+  [400, 200],
+  [300, 450],
+  [100, 450],
+  [200, 210],
+].map(([x, y]) => ({ x, y }))
+
+const RENDER_IMAGE_SPEED_PX_SEC = 100 // px/sec
+
+const getSignalBetween2Points = (p1: Point, p2: Point) => {
+  const xDiff = p2.x - p1.x
+  const yDiff = p2.y - p1.y
+  const dist = Math.floor(distance(p1, p2))
+
+  return Array.from({ length: dist }).map((_, idx) => ({
+    x: p1.x + (xDiff / dist) * idx,
+    y: p1.y + (yDiff / dist) * idx,
+  }))
+}
+
+const getSignalFromPoints = (points: Point[]) => {
+  const [firstPoint, ...restPoints] = points
+
+  let prevPoint = firstPoint
+  const v = restPoints.map(p => {
+    const ret = getSignalBetween2Points(prevPoint, p)
+    prevPoint = p
+    return ret
+  })
+  return v.flat()
+}
+
+// ----------------------------
+// ----------------------------
+// ----------------------------
+const renderChart = (
+  grid: { leftTop: Point; width: number; height: number },
+  values: number[][],
+  {
+    sampleRate,
+    xAxisScaleFactor,
+    yAxisScaleFactor,
+  }: { sampleRate: number; xAxisScaleFactor: number; yAxisScaleFactor: number }
+) => {
+  drawRect(grid.leftTop, grid.width, grid.height, {
+    color: '#fff',
+  })
+
+  const yZeroCoords = { x: grid.leftTop.x, y: grid.leftTop.y + grid.height / 2 }
+
+  // center 0 point
+  drawLine(
+    yZeroCoords,
+    { x: grid.leftTop.x + grid.width, y: grid.leftTop.y + grid.height / 2 },
+    { color: '#DDD' }
+  )
+
+  // show 1 sec delimiter
+  const COUNT_OF_LINES_BETWEEN_SECS = 10
+  Array.from({
+    length: (Math.floor(grid.width / sampleRate) + 1) * 10,
+  })
+    .map((_, i) => (i * xAxisScaleFactor) / COUNT_OF_LINES_BETWEEN_SECS)
+    .map((i, index) => {
+      const isSec = index % COUNT_OF_LINES_BETWEEN_SECS === 0
+      drawLine(
+        {
+          x: yZeroCoords.x + i * sampleRate,
+          y: yZeroCoords.y - grid.height / 2,
+        },
+        {
+          x: yZeroCoords.x + i * sampleRate,
+          y: yZeroCoords.y + grid.height / 2,
+        },
+        { color: isSec ? 'red' : '#DDD', width: isSec ? 3 : 1 }
+      )
+    })
+
+  values.forEach((points, valIndex) => {
+    let [prevPoint, ...restPoints] = points
+      .map(x => x * yAxisScaleFactor)
+      // add relative position to the yZeroLine
+      .map((v, i) => ({
+        x: yZeroCoords.x + i * xAxisScaleFactor,
+        y: yZeroCoords.y - v, // there is `-` sign because of display is mirror rotatable by Y axis
+      }))
+
+    restPoints.forEach(p => {
+      drawLine(prevPoint, p, {
+        color: valIndex === 0 ? 'blue' : '#BBB',
+      })
+      prevPoint = p
+    })
+  })
+}
+
+const computeFT = (wave: number[]) => {
+  var inputWaveform = new Float32Array(wave)
+  var fftOutputSpectrum = ft(inputWaveform) as number[]
+  return fftOutputSpectrum
+}
+
+// TODO: add transpose matrix to keep the code simpler... but I don't care tbh
+const getSumOfFns = (fns: number[][]) => {
+  return fns[0].map((v, index) => {
+    let sum = 0
+    for (let i = 0; i < fns.length; i++) {
+      sum += fns[i][index]
+    }
+    return sum
+  })
+}
+
+// TODO: xAxisScaleFactor does not scale background
+const renderSquareWave = () => {
+  // The sampling rate refers to the number of samples per second.
+  const SAMPLE_RATE = 200
+  // the number of waves that pass a fixed point in unit time (1sec)
+  const frequency = 1
+  const SIZE = 2 ** 11 // 2048
+
+  const getSinSquareWavePartSample = (order: number) =>
+    Array.from(
+      { length: SIZE },
+      (_, i) => Math.sin(frequency * order * Math.PI * 2 * (i / SAMPLE_RATE)) * (1 / order)
+    )
+
+  const APPROXIMATION_COUNT = 20
+  const squareWaveFns = Array.from({ length: APPROXIMATION_COUNT }).map((_, i) =>
+    getSinSquareWavePartSample(1 + i * 2)
+  )
+
+  const sumFn = getSumOfFns(squareWaveFns)
+  renderChart(
+    {
+      leftTop: { x: 10, y: 610 },
+      width: SIZE,
+      height: 190,
+    },
+    [sumFn, ...squareWaveFns],
+    { sampleRate: SAMPLE_RATE, xAxisScaleFactor: 1, yAxisScaleFactor: 80 }
+  )
+
+  const fftOutputSpectrum = computeFT(sumFn)
+
+  renderChart(
+    {
+      leftTop: { x: 10, y: 810 },
+      width: SIZE,
+      height: 190,
+    },
+    [fftOutputSpectrum],
+    { sampleRate: SAMPLE_RATE, xAxisScaleFactor: 3, yAxisScaleFactor: 100 }
+  )
+
+  const fns2 = getFTMetadata(fftOutputSpectrum, {
+    size: SIZE,
+    sampleRate: SAMPLE_RATE,
+    peaksCount: APPROXIMATION_COUNT,
+  })
+
+  const fnsData = fns2.map(i =>
+    getSinusFunctionSamples({
+      size: SIZE,
+      sampleRate: SAMPLE_RATE,
+      frequency: i.frequency,
+      amplitude: i.amplitude,
+    })
+  )
+
+  const dataToEngine = fns2.map(i => ({
+    radius: i.amplitude,
+    rotationPerSecond: 1 / i.frequency,
+    touchPointAngle: 0,
+  }))
+
+  // square wave
+  xd(dataToEngine)
+
+  // console.log(JSON.stringify(fns2, null, 2))
+
+  renderChart(
+    {
+      leftTop: { x: 10, y: 1010 },
+      width: SIZE,
+      height: 190,
+    },
+
+    // TODO: add sum function
+    [getSumOfFns(fnsData) /*, ...fnsData*/],
+    { sampleRate: SAMPLE_RATE, xAxisScaleFactor: 1, yAxisScaleFactor: 80 }
+  )
+}
+
+const getSinusFunctionSamples = (a: {
+  size: number
+  // the number of samples per second. (1 sec = red line in the chart)
+  sampleRate: number
+  // the number of waves that pass a fixed point in unit time (1sec)
+  frequency: number
+  amplitude: number
+}) =>
+  Array.from(
+    { length: a.size },
+    // todo: check this formula if its working well
+    (_, i) => Math.sin(a.frequency * Math.PI * 2 * (i / a.sampleRate)) * a.amplitude
+  )
+
+const renderBasicComposedSins = () => {
+  // const amplitude = 1
+  // the number of samples per second. (1 sec = red line in the chart)
+  const SAMPLE_RATE = 2 ** 10 / 3
+  // the number of waves that pass a fixed point in unit time (1sec)
+  // const frequency = 1
+  const SIZE = 2 ** 10 // 2048
+  const fns = [
+    getSinusFunctionSamples({
+      size: SIZE,
+      sampleRate: SAMPLE_RATE,
+      frequency: 10,
+      amplitude: 1.2,
+    }),
+
+    getSinusFunctionSamples({
+      size: SIZE,
+      sampleRate: SAMPLE_RATE,
+      frequency: 20,
+      amplitude: 0.5,
+    }),
+
+    getSinusFunctionSamples({
+      size: SIZE,
+      sampleRate: SAMPLE_RATE,
+      frequency: 15,
+      amplitude: 1,
+    }),
+  ]
+  const sumFn = getSumOfFns(fns)
+
+  renderChart(
+    {
+      leftTop: { x: 10, y: 0 },
+      width: SIZE,
+      height: 190,
+    },
+
+    [sumFn /*, ...fns*/],
+    { sampleRate: SAMPLE_RATE, xAxisScaleFactor: 2, yAxisScaleFactor: 20 }
+  )
+
+  const fftOutputSpectrum = computeFT(sumFn)
+
+  // height of the bar === amplitude
+  // X coords === frequency
+  renderChart(
+    {
+      leftTop: { x: 10, y: 210 },
+      width: SIZE,
+      height: 190,
+    },
+
+    [fftOutputSpectrum],
+    { sampleRate: SAMPLE_RATE, xAxisScaleFactor: 1, yAxisScaleFactor: 50 }
+  )
+
+  const fns2 = getFTMetadata(fftOutputSpectrum, {
+    size: SIZE,
+    sampleRate: SAMPLE_RATE,
+    peaksCount: fns.length,
+  })
+
+  const dataToEngine = fns2.map(i => ({
+    radius: i.amplitude,
+    // TODO: refactor info frequency
+    rotationPerSecond: 1 / i.frequency,
+  }))
+
+  // console.log('analyze signal')
+  // console.log(JSON.stringify(dataToEngine, null, 2))
+  // basic function
+  // xd(dataToEngine)
+
+  const fnsData = fns2.map(i =>
+    getSinusFunctionSamples({
+      size: SIZE,
+      sampleRate: SAMPLE_RATE,
+      frequency: i.frequency,
+      amplitude: i.amplitude,
+    })
+  )
+
+  renderChart(
+    {
+      leftTop: { x: 10, y: 410 },
+      width: SIZE,
+      height: 190,
+    },
+
+    // TODO: add sum function
+    [getSumOfFns(fnsData) /*, ...fnsData*/],
+    { sampleRate: SAMPLE_RATE, xAxisScaleFactor: 2, yAxisScaleFactor: 20 }
+  )
+}
+
+// not sure if it workings well => will be better to have only peaks
+const getSignalPeaks = (ftWave: number[]) => {
+  return (
+    ftWave
+      .filter((amplitude, index) => {
+        const prevVal = ftWave[index - 1] ?? 0
+        const nextVal = ftWave[index + 1] ?? 0
+        const isPeak = amplitude > prevVal && amplitude > nextVal
+        return isPeak
+      })
+      // TODO: +1??? there is no 0 element i guess
+      .map((amplitude, index) => ({ amplitude, xPosition: index + 1 }))
+      .sort((a, b) => a.amplitude - b.amplitude)
+      .reverse()
+  )
+}
+
+// How to read data:
+
+//length of the array / sample rate
+
+// the peak means how many frequency will be done in that time
+
+// https://towardsdatascience.com/insight-to-the-fourier-transform-and-the-simple-implementation-of-it-eee293317efd
+// size === duration of the sound
+const getFrequencyFromFT = (a: { xPosition: number; size: number; sampleRate: number }) =>
+  a.xPosition / (a.size / a.sampleRate)
+
+const getFTMetadata = (
+  ftWave: number[],
+  a: {
+    size: number
+    sampleRate: number
+    peaksCount: number
+  }
+) => {
+  const peaks = getSignalPeaks(ftWave)
+  // console.log(peaks)
+  // const sortedByValue = ftWave
+  //   .map((amplitude, index) => ({ amplitude, index }))
+  //   .sort((a, b) => a.amplitude - b.amplitude)
+  //   .reverse()
+
+  // TODO: how many values
+
+  const data = peaks.slice(0, a.peaksCount).map(f => ({
+    amplitude: f.amplitude,
+    frequency: getFrequencyFromFT({
+      xPosition: f.xPosition,
+      size: a.size,
+      sampleRate: a.sampleRate,
+    }),
+  }))
+  return data
+}
+
+const initRenderUI = () => {
+  setCanvasSize()
+  clearCanvas()
+  drawRect({ x: 0, y: 0 }, view.width, view.height, {
+    color: '#000',
+  })
+
+  renderSquareWave()
+  renderBasicComposedSins()
+}
+
+initRenderUI()
+// window.onload = initRenderUI
+
+// ---------------
+
+export {}
---
+++ src/renderFFTEngine.ts
@@ -0,0 +1,302 @@
+type Point = {
+  x: number
+  y: number
+}
+
+// --------------------------- canvas utils ---------------------------
+const drawLine = (point1: Point, point2: Point, { color = 'black', width = 2 } = {}) => {
+  ctx.beginPath()
+  ctx.moveTo(point1.x, point1.y)
+  ctx.lineTo(point2.x, point2.y)
+  ctx.lineWidth = width
+  ctx.strokeStyle = color
+  ctx.stroke()
+}
+
+const drawRect = (point: Point, width: number, height: number, { color = 'black' } = {}) => {
+  ctx.beginPath()
+  ctx.rect(point.x, point.y, width, height)
+  ctx.fillStyle = color
+  ctx.fill()
+}
+
+const drawPoint = (point: Point, { color = 'black', size = 2 } = {}) => {
+  drawRect(point, size, size, { color })
+  // const _size = size === 1 ? 2 : size
+  // // if (size > 1) {
+  // // ctx.fillStyle = color
+  // ctx.rect(point.x, point.y, _size, _size)
+  // ctx.strokeStyle = color
+  // // ctx.fill()
+  // ctx.stroke()
+  // } else {
+  //   drawLine(point, { x: point.x + size, y: point.y + size }, { color, width: 1 })
+  // }
+}
+
+const drawCircle = (center: Point, radius: number, { color = 'black', width = 2 } = {}) => {
+  ctx.beginPath()
+  ctx.arc(center.x, center.y, radius, 0, 2 * Math.PI)
+  ctx.strokeStyle = color
+  ctx.lineWidth = width
+  ctx.stroke()
+}
+
+const view = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+const clearCanvas = () => ctx.clearRect(0, 0, view.width, view.height)
+
+var c = document.getElementById('myCanvas2')!
+
+const setCanvasSize = () => {
+  // @ts-expect-error
+  c.width = view.width
+  // @ts-expect-error
+  c.height = view.height
+}
+
+// @ts-expect-error
+var ctx = c.getContext('2d')
+
+let timeMs = Date.now()
+let startTimeMs = Date.now()
+// little abstraction to enable stop and continue the animation
+let stopProgrammeTime = 0
+const fps = 33
+
+const normalizeIntoInterval = (val: number, max: number, min: number) => (val - min) / (max - min)
+
+let endPoints = [] as Point[]
+
+type FourierCircle = {
+  radius: number
+  rotationPerSecond: number
+  touchPointAngle: number
+}
+
+const drawFourierCircle = (time: number, startPoint: Point, meta: FourierCircle) => {
+  drawCircle(startPoint, meta.radius, { color: '#BBF' })
+
+  // draw rotating point on Line
+  const percentageAngle = normalizeIntoInterval(
+    time % (meta.rotationPerSecond * 1_000),
+    meta.rotationPerSecond * 1_000,
+    0
+  )
+
+  // minus sign to clockwise rotation
+  const angleRad = percentageAngle * (2 * Math.PI) + meta.touchPointAngle + Math.PI / 2
+  const endPoint = {
+    x: startPoint.x + Math.sin(angleRad) * meta.radius,
+    y: startPoint.y + Math.cos(angleRad) * meta.radius,
+  }
+
+  drawLine(startPoint, endPoint, { color: '#66F' })
+  return endPoint
+}
+
+const renderFourierCircles = (time: number, startPoint: Point, fftCircles: FourierCircle[]) => {
+  const [firstCircle, ...restCircles] = fftCircles
+  let lastEndpoint = drawFourierCircle(time, startPoint, firstCircle)
+
+  restCircles.forEach(c => {
+    const newEndPoint = drawFourierCircle(time, lastEndpoint, c)
+    lastEndpoint = newEndPoint
+  })
+
+  return lastEndpoint
+}
+
+const renderFourier2Circles = (
+  time: number,
+  fftCirclesX: FourierCircle[],
+  fftCirclesY: FourierCircle[]
+) => {
+  const endPointY = renderFourierCircles(
+    time,
+    {
+      x: view.width / 2 - 500,
+      y: view.height / 2,
+    },
+    fftCirclesY
+  )
+
+  const endPointX = renderFourierCircles(
+    time,
+    {
+      x: view.width / 2,
+      y: view.height / 2 - 400,
+    },
+    fftCirclesX
+  )
+
+  const drawPoint = {
+    x: endPointX.x,
+    y: endPointY.y,
+  }
+
+  endPoints = [drawPoint, ...endPoints]
+
+  drawLine(endPointY, drawPoint, { color: '#555' })
+  drawLine(endPointX, drawPoint, { color: '#555' })
+
+  // keep just 40 end points
+  endPoints = endPoints.reverse().slice(-1200).reverse()
+  // + 1 => depends on frameRate
+  endPoints = endPoints.map(i => ({ ...i, x: i.x + 3 }))
+
+  // endPoints = endPoints.map((p, i) => ({ x: p.x - i, y: p.y }))
+
+  endPoints.forEach((p, i) => {
+    const prevPoint = endPoints[i - 1]
+    // uncomment to show nice line
+    const widthByTime = 4 // (endPoints.length - i) / 8
+    drawLine(prevPoint || p, p, { color: 'red', width: widthByTime })
+  })
+}
+// ------------------------------------------------------------
+
+// ------------------------------------------------------------
+
+// square wave
+// amplitude
+
+const fourierTransformData = Array.from({ length: 10 })
+  .map((_, i) => ({
+    radius: 1 / (i * 2 + 1),
+    freq: i * 2 + 1,
+  }))
+  .map(i => ({ ...i, rotationPerSecond: 1 / i.freq }))
+
+const RADIUS_RESIZE = 100
+const examples = {
+  a: [
+    {
+      radius: 10,
+      rotationPerSecond: -1,
+      touchPointAngle: Math.PI / 2,
+    },
+    {
+      radius: 10,
+      rotationPerSecond: 1,
+      touchPointAngle: Math.PI / 2,
+    },
+  ],
+  b: [
+    ...fourierTransformData,
+    // {
+    //   radius: 0.67201625231031,
+    //   rotationPerSecond: 1.024,
+    //   touchPointAngle: 0,
+    // },
+    // {
+    //   radius: 0.6667146789544217,
+    //   rotationPerSecond: 1.1377777777777778,
+    //   touchPointAngle: 0,
+    // },
+    // {
+    //   radius: 0.22667382195124688,
+    //   rotationPerSecond: 0.3303225806451613,
+    //   touchPointAngle: 0,
+    // },
+    // {
+    //   radius: 0.20723312387496987,
+    //   rotationPerSecond: 0.9309090909090909,
+    //   touchPointAngle: 0,
+    // },
+    // {
+    //   radius: 0.19694730833188054,
+    //   rotationPerSecond: 0.3413333333333333,
+    //   touchPointAngle: 0,
+    // },
+    // {
+    //   radius: 0.14814951567985596,
+    //   rotationPerSecond: 0.2048,
+    //   touchPointAngle: 0,
+    // },
+    // {
+    //   radius: 0.14414789446265455,
+    //   rotationPerSecond: 1.28,
+    //   touchPointAngle: 0,
+    // },
+    // {
+    //   radius: 0.11702512194550076,
+    //   rotationPerSecond: 0.2007843137254902,
+    //   touchPointAngle: 0,
+    // },
+  ].map(i => ({
+    ...i,
+    radius: i.radius * RADIUS_RESIZE,
+    rotationPerSecond: -(i.rotationPerSecond * 2),
+    touchPointAngle: 0,
+    // touchPointAngle: -Math.PI / 3,
+    // rotationPerSecond: i.rotationPerSecond / 3,
+  })),
+}
+
+let runProgram = true
+
+const renderCycleTick = () => {
+  timeMs = Date.now() - startTimeMs // - stopProgrammeTime
+  // console.log(timeMs)
+
+  setTimeout(() => {
+    if (!runProgram) {
+      // stopProgrammeTime = Date.now() - timeMs
+    } else {
+      clearCanvas()
+      drawRect({ x: 0, y: 0 }, view.width, view.height, {
+        color: 'black',
+      })
+
+      // console.log(timeMs)
+      renderFourier2Circles(timeMs, examples.a, examples.b)
+      // runProgram = false
+    }
+
+    // uncomment to step it
+    // runProgram = false
+
+    renderCycleTick()
+  }, 1000 / fps)
+}
+
+const initRenderUI = () => {
+  startTimeMs = Date.now()
+  setCanvasSize()
+}
+
+document.addEventListener('keydown', e => {
+  if (e.code === 'Space') {
+    runProgram = !runProgram
+  }
+})
+
+// TODO: resize is not working yet
+window.addEventListener('resize', e => {
+  view.width = window.innerWidth
+  view.height = window.innerHeight
+  // initRenderUI()
+  setCanvasSize()
+  // renderCycleTick()
+})
+
+// window.onload = initRenderUI
+initRenderUI()
+
+// ---------------
+
+export const xd = (data: any[]) => {
+  console.log(data)
+  examples.b = data.map(i => ({
+    ...i,
+    radius: i.radius * RADIUS_RESIZE * 2,
+    rotationPerSecond: i.rotationPerSecond / 2, // * 20, //  / 8,
+    touchPointAngle: 0,
+  }))
+  startTimeMs = Date.now() // - startTimeMs
+  renderCycleTick()
+}
